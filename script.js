7e3cabfa006d08276d18a45df78aed5ccf429662
Weverton Matheus <wevertoncouydev@gmail.com>
2023-02-10 22:22:05 -0400

    add countdown timer

--- script.js
+++ script.js
@@ -6,15 +6,47 @@ const stopBtn = document.querySelector('.stop-btn')
 const soundOnBtn = document.querySelector('.sound-on')
 const soundOffBtn = document.querySelector('.sound-off')
 
-let minutes
+let timerTimeOut
 const minutesDisplay = document.querySelector('.minutes')
 const secondsDisplay = document.querySelector('.seconds')
+let minutes = +minutesDisplay.textContent
 
+function resetPomodoro() {
+  updateTimerDisplay(minutes, 0)
+  clearTimeout(timerTimeOut)
+}
 
+function updateTimerDisplay(minutes, seconds) {
+  minutesDisplay.textContent = (minutes < 10 ? '0' : '') + minutes
+  secondsDisplay.textContent = (seconds < 10 ? '0' : '') + seconds
+  console.log('Minutes: ', minutes, 'Seconds: ', seconds)
+}
+
+function countdown() {
+  timerTimeOut = setTimeout(() => {
+    let seconds = +secondsDisplay.textContent
+    let minutes = +minutesDisplay.textContent
+
+    updateTimerDisplay(minutes, 0)
+    if (minutes <= 0) {
+      resetControls()
+      return
+    }
+
+    if (seconds <= 0) {
+      seconds = 12
+      --minutes
+    }
+
+    updateTimerDisplay(minutes, seconds - 1)
+
+    countdown()
+  }, 1000)
+}
 
 playBtn.addEventListener('click', play)
 pauseBtn.addEventListener('click', pause)
-stopBtn.addEventListener('click', stops)
+stopBtn.addEventListener('click', stopControl)
 
 soundOnBtn.addEventListener('click', soundOn)
 soundOffBtn.addEventListener('click', soundOff)
@@ -24,23 +56,26 @@ stopwatchBtn.addEventListener('click', stopwatch)
 function play() {
   playBtn.classList.add('hide')
   pauseBtn.classList.remove('hide')
-
   stopwatchBtn.classList.add('hide')
   stopBtn.classList.remove('hide')
 
+  countdown()
 }
 
 function pause() {
   playBtn.classList.remove('hide')
   pauseBtn.classList.add('hide')
+
+  clearTimeout(timerTimeOut)
 }
 
-function stops() {
+function stopControl() {
   stopBtn.classList.add('hide')
   stopwatchBtn.classList.remove('hide')
-
   playBtn.classList.remove('hide')
   pauseBtn.classList.add('hide')
+
+  resetPomodoro()
 }
 
 function soundOn() {
@@ -54,6 +89,13 @@ function soundOff() {
 }
 
 function stopwatch() {
-  minutes = prompt('Quantos minutos?')
-  minutesDisplay.textContent = minutes
+  inputMinutes = prompt('Digite Tempo do Pomodoro?')
+  if (isNaN(inputMinutes) || !inputMinutes) {
+    resetPomodoro()
+    alert('Digite somente numeros!')
+    return
+  }
+
+  minutes = inputMinutes
+  updateTimerDisplay(minutes, 0)
 }
